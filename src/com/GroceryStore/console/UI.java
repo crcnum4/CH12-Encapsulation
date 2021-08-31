acd6e48627e87df6572ce758073253ce5fa207de
Cliff Choiniere <cliffchoiniere@cliffs-mbp.myfiosgateway.com>
2021-08-30 21:30:54 -0400

    product abstract class, store class, started ui class

---
+++ src/com/GroceryStore/console/UI.java
@@ -0,0 +1,38 @@
+package com.GroceryStore.console;
+
+import com.GroceryStore.Store;
+
+import java.util.Scanner;
+
+public class UI {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void welcome(String name) {
+        System.out.println("Welcome to " + name + "!");
+    }
+
+    public static void optionMenu() {
+        System.out.println("What would you like to do?");
+        System.out.println("1. add product to inventory");
+        System.out.println("2. throw away a product");
+        System.out.println("3. list products available");
+        System.out.println("4. sell a product");
+        System.out.println("5. quit");
+    }
+
+    public static void start(Store store) {
+        welcome(store.getName());
+        optionMenu();
+        int choice = getInt(1, 5, "Enter selection between 1 and 5:");
+    }
+
+    public static int getInt(int min, int max, String prompt) {
+        int option;
+        do {
+            System.out.println(prompt);
+            String input = scanner.next();
+            option = Integer.parseInt(input);
+        } while (option < min || option > max);
+        return option;
+    }
+}
